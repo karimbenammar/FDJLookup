2041b71d258ae81dde851e4d110df4fa4422b8ee
Karim Ben Ammar <29929026+karimbenammar@users.noreply.github.com>
2023-06-13 14:58:59 +0100

    chore: move dependencies to variables

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,34 +51,33 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.10.1")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
 
     // Compose
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.06.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation(platform(Libs.composeBOM))
+    implementation(Libs.composeUI)
+    implementation(Libs.composeActivities)
+    implementation(Libs.composeGraphics)
+    implementation(Libs.composeToolingPreview)
+    implementation(Libs.composeMaterial3)
+    implementation(Libs.composeNavigation)
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.46.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.46.1")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation(Libs.hilt)
+    implementation(Libs.hilt_compose)
+    kapt(Libs.hilt_compiler)
+    kapt(Libs.hilt_kapt)
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(Libs.retrofit2)
+    implementation(Libs.retrofit2_gson_converter)
 
     // Landscapist
-    implementation("com.github.skydoves:landscapist-glide:2.2.1")
-    implementation("com.github.skydoves:landscapist-animation:2.2.1")
-    implementation("com.github.skydoves:landscapist-placeholder:2.2.1")
+    implementation(Libs.landscapist_glide)
+    implementation(Libs.landscapist_animation)
+    implementation(Libs.landscapist_placeholder)
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,32 @@
+object Versions {
+    const val compose_bom_version = "2023.06.00"
+    const val compose_navigation_version = "2.5.3"
+    const val hilt_version = "2.46.1"
+    const val hilt_compose_version = "1.0.0"
+    const val hilt_compiler_version = "1.0.0"
+    const val retrofit2_version = "2.9.0"
+    const val landscapist_version = "2.2.1"
+}
+
+object Libs {
+
+    const val composeBOM = "androidx.compose:compose-bom:${Versions.compose_bom_version}"
+    const val composeUI = "androidx.compose.ui:ui"
+    const val composeActivities = "androidx.activity:activity-compose"
+    const val composeGraphics = "androidx.compose.ui:ui-graphics"
+    const val composeToolingPreview = "androidx.compose.ui:ui-tooling-preview"
+    const val composeMaterial3 = "androidx.compose.material3:material3"
+    const val composeNavigation = "androidx.navigation:navigation-compose:${Versions.compose_navigation_version}"
+
+    const val hilt = "com.google.dagger:hilt-android:${Versions.hilt_version}"
+    const val hilt_compose = "androidx.hilt:hilt-navigation-compose:${Versions.hilt_compose_version}"
+    const val hilt_compiler = "com.google.dagger:hilt-android-compiler:${Versions.hilt_version}"
+    const val hilt_kapt = "androidx.hilt:hilt-compiler:${Versions.hilt_compiler_version}"
+
+    const val retrofit2 = "com.squareup.retrofit2:retrofit:${Versions.retrofit2_version}"
+    const val retrofit2_gson_converter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit2_version}"
+
+    const val landscapist_glide = "com.github.skydoves:landscapist-glide:${Versions.landscapist_version}"
+    const val landscapist_animation = "com.github.skydoves:landscapist-animation:${Versions.landscapist_version}"
+    const val landscapist_placeholder = "com.github.skydoves:landscapist-placeholder:${Versions.landscapist_version}"
+}
